b1d2b61a8a79a45c392ea4ec9ed523db02983765
SanjarOrozbekov <sanchshb@gmail.com>
2025-05-20 13:41:40 +0600

    presentation task

---
+++ src/SessionTask.sql
@@ -0,0 +1,249 @@
+create table people (
+                        id INT,
+                        first_name VARCHAR(50),
+                        last_name VARCHAR(50),
+                        email VARCHAR(50),
+                        gender VARCHAR(50),
+                        age int,
+                        language VARCHAR(50)
+);
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (1, 'Joela', 'Cattonnet', 'jcattonnet0@nasa.gov', 'Female', 19, 'Somali');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (2, 'Dorothy', 'Onions', 'donions1@stumbleupon.com', 'Female', 20, 'Filipino');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (3, 'Conrado', 'Batstone', 'cbatstone2@clickbank.net', 'Male', 34, 'Irish Gaelic');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (4, 'Andre', 'MacGillespie', 'amacgillespie3@amazonaws.com', 'Male', 12, 'English');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (5, 'Vlad', 'Elson', 'velson4@reference.com', 'Male', 15, 'Montenegrin');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (6, 'Arman', 'Matejic', 'amatejic5@linkedin.com', 'Male', 19, 'Papiamento');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (7, 'Perkin', 'Altham', 'paltham6@hibu.com', 'Male', 45, 'Zulu');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (8, 'Nathan', 'Dommett', 'ndommett7@storify.com', 'Male', 23, 'Polish');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (9, 'Rahal', 'Dericot', 'rdericot8@twitter.com', 'Female', 48, 'Armenian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (10, 'Alonso', 'Marusik', 'amarusik9@blogtalkradio.com', 'Male', 67, 'Swahili');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (11, 'Clari', 'Wipper', 'cwippera@twitter.com', 'Female', 18, 'Montenegrin');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (12, 'Andonis', 'Pointon', 'apointonb@soundcloud.com', 'Male', 20, 'Ndebele');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (13, 'Orelia', 'Besson', 'obessonc@hubpages.com', 'Female', 19, 'Czech');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (14, 'Tulley', 'Swann', 'tswannd@tinyurl.com', 'Male', 35, 'Zulu');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (15, 'Kirstin', 'Driffill', 'kdriffille@smh.com.au', 'Female', 20, 'Guaraní');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (16, 'Garvy', 'Sherry', 'gsherryf@nyu.edu', 'Male', 26, 'Punjabi');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (17, 'Morgen', 'Snoxell', 'msnoxellg@bandcamp.com', 'Male', 28, 'Nepali');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (18, 'Janine', 'Curl', 'jcurlh@slate.com', 'Female', 27, 'Kyrgyz');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (19, 'Lilllie', 'Barnsdall', 'lbarnsdalli@disqus.com', 'Female', 20, 'Bulgarian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (20, 'Dynah', 'Tincombe', 'dtincombej@sciencedaily.com', 'Female', 24, 'Khmer');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (21, 'Sacha', 'Matthius', 'smatthiusk@ca.gov', 'Female', 22, 'Arabic');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (22, 'Raynard', 'Gerrans', 'rgerransl@i2i.jp', 'Male', 21, 'Ndebele');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (23, 'Tom', 'Machent', 'tmachentm@mlb.com', 'Male', 29, 'Polish');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (24, 'Fabien', 'Forrest', 'fforrestn@house.gov', 'Male', 47, 'Aymara');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (25, 'Mickie', 'Coal', 'mcoalo@seesaa.net', 'Male', 25, 'Aymara');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (26, 'Tana', 'Weeds', 'tweedsp@shinystat.com', 'Female', 36, 'Montenegrin');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (27, 'Ilka', 'McMichan', 'imcmichanq@bigcartel.com', 'Female', 39, 'Swahili');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (28, 'Mychal', 'Mapplebeck', 'mmapplebeckr@acquirethisname.com', 'Male', 30, 'Filipino');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (29, 'Melody', 'Beck', 'mbecks@pcworld.com', 'Female', 37, 'Kazakh');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (30, 'Lezley', 'Summerbell', 'lsummerbellt@cocolog-nifty.com', 'Male', 56, 'Maltese');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (31, 'Gaye', 'Withringten', 'gwithringtenu@discovery.com', 'Female', 43, 'Swedish');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (32, 'Leonelle', 'Baggallay', 'lbaggallayv@i2i.jp', 'Female', 20, 'Greek');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (33, 'Shir', 'Trevarthen', 'strevarthenw@pagesperso-orange.fr', 'Female', 29, 'Swahili');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (34, 'Ddene', 'Hamlen', 'dhamlenx@theguardian.com', 'Female', 76, 'Croatian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (35, 'Juditha', 'Yellowlea', 'jyellowleay@cafepress.com', 'Female', 54, 'Persian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (36, 'Danika', 'Stinson', 'dstinsonz@vinaora.com', 'Female', 38, 'Bosnian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (37, 'Garrek', 'Bartalini', 'gbartalini10@bing.com', 'Male', 50, 'Kazakh');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (38, 'Seumas', 'Silman', 'ssilman11@usda.gov', 'Male', 47, 'Indonesian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (39, 'Julius', 'Leftley', 'jleftley12@angelfire.com', 'Male', 44, 'Lithuanian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (40, 'Jeanine', 'Sprankling', 'jsprankling13@phpbb.com', 'Female', 34, 'Romanian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (41, 'Clywd', 'Cowles', 'ccowles14@prweb.com', 'Male', 75, 'Gagauz');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (42, 'Rahel', 'Vasilevich', 'rvasilevich15@ustream.tv', 'Female', 54, 'Zulu');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (43, 'Alexandr', 'Obell', 'aobell16@marriott.com', 'Male', 14, 'Filipino');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (44, 'Becky', 'Learoyde', 'blearoyde17@networkadvertising.org', 'Female', 35, 'Georgian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (45, 'Giustino', 'Putt', 'gputt18@vistaprint.com', 'Male', 43, 'Estonian');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (46, 'Uta', 'Tonbridge', 'utonbridge19@123-reg.co.uk', 'Female', 55, 'Chinese');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (47, 'Granthem', 'Diver', 'gdiver1a@un.org', 'Male', 76, 'Bengali');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (48, 'Livvy', 'Akister', 'lakister1b@nhs.uk', 'Female', 45, 'Burmese');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (49, 'Valle', 'Greenwood', 'vgreenwood1c@tmall.com', 'Male', 65, 'Guaraní');
+insert into people (id, first_name, last_name, email, gender, age, language)
+values (50, 'Nicole', 'Coey', 'ncoey1d@mail.ru', 'Female', 42, 'German');
+
+--1.Вывести все строки из таблицы
+SELECT *
+FROM people;
+
+--2.Обновить электронную почту конкретного человека
+UPDATE people
+SET email = 'kyrgyz@gmail.com'
+WHERE id = 1;
+
+INSERT INTO people (id, first_name, last_name, email, gender, age, language)
+VALUES (51,'Sanjar','Orozobekov','sake@gmail.com','MALE',18,'Kyrgyz');
+
+DELETE FROM people
+WHERE id = 10;
+
+SELECT count(*)
+FROM people;
+
+SELECT avg(age)
+FROM people;
+
+SELECT max(age)
+FROM people;
+
+SELECT sum(age)
+FROM people;
+
+SELECT min(age)
+FROM people;
+
+SELECT avg(age),gender
+FROM people
+GROUP BY gender;
+
+SELECT first_name,age
+FROM people
+WHERE language = 'English';
+
+SELECT count(*)
+FROM people
+WHERE language = 'Kazakh';
+
+SELECT last_name,email,id
+FROM people
+WHERE gender = 'Female';
+
+SELECT concat(first_name , ' ' , last_name)as full_name,age,gender
+FROM people
+WHERE gender = 'Male' and age >25;
+
+SELECT count(*)
+FROM people
+WHERE first_name
+    ILIKE ('A%');
+
+SELECT *
+FROM people
+ORDER BY email ASC;
+
+SELECT *
+FROM people
+         LIMIT 20 OFFSET 5;
+
+SELECT count(*),language
+FROM people
+GROUP BY language;
+
+SELECT avg(age)
+FROM people
+GROUP BY language;
+
+-- 21. Найти самый распространенный язык среди женщин
+SELECT max(language)
+FROM people
+WHERE gender = 'Female'
+    LIMIT 1;
+-- 22. Обновить язык для людей, чей возраст между 3 и 10 годами, на 'French'
+UPDATE people
+SET language = 'French'
+WHERE age BETWEEN 3 and 20;
+-- 23. Отобразите всех мужчин говорящих на Tamil, Kazakh, Nepali и Russian
+SELECT * FROM people
+WHERE gender = 'Male' and language = 'Tamil' or language = 'Kazakh' or language = 'Nepali' or language = 'Russian';
+
+-- 24. Сгруппируте всех по языку на котором они говорят
+SELECT language,count(*)
+FROM people
+GROUP BY language;
+-- 25. Отобразите записи фамилии которых содержат только 7 символов и
+-- отсортируйте их по айди
+SELECT last_name
+FROM people
+WHERE length(last_name) = 7
+ORDER BY id;
+
+-- 26. Отобразите все записи кроме говорящих на French языке
+SELECT *
+FROM people
+WHERE language != 'French';
+
+-- 27. Отобразите 5 самых молодых людей у которых почта заканчивается .com
+SELECT *
+FROM people
+ORDER BY age
+    LIMIT 5;
+-- 28. Отобразите только те записи из таблицы которым есть 18 и которых
+-- почта заканчивается на .com или .uk
+SELECT *
+FROM people
+WHERE age > 18
+  AND email
+    ILIKE ('%.com')
+OR email
+ILIKE ('%.uk');
+-- 29. Отобразите все записи, кроме родившихся с 1993 по 2000 года
+SELECT *
+FROM people
+WHERE age < 25
+   OR age > 32;
+-- 30. Отобразите уникальные языки
+SELECT Distinct language
+FROM people;
+-- 31. Отобразите записи где в имени или в фамилии есть буква ‘a’ и год
+-- рождение с 1969 по 2005 год
+SELECT *
+FROM people
+WHERE (first_name ILIKE '%a%' or last_name ILIKE '%a%')
+  and age NOT BETWEEN 20 and 56;
+-- 32. Отобразите айди, имя, почту и возврст каждого человека и отсортируйте
+-- по возврасту в убывающем порядке
+SELECT id,first_name,email,age
+FROM people
+ORDER BY age DESC ;
+-- 33. Удалить людей младще 3 лет
+DELETE FROM people
+where age < 3;
+
+
+
